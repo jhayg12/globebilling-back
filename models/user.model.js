e7ff6bd27877596f1f04db48566de5ff15905b23
Jayson Suyat <jayson@iscale-solutions.com>
2018-07-16 13:06:20 +0800

    Update Users Schema

--- models/user.model.js
+++ models/user.model.js
@@ -7,8 +7,10 @@ let user = Schema({
     fullname: {type: String, required: true},
     email: {type: String, trim: true, unique: true},
     password: {type: String, required: true},
-    role: {type: Schema.Types.ObjectId, ref: 'Roles'},
-    status: {type: Number, default: 1}
+    role: {type: Schema.Types.ObjectId, ref: 'Role', default: null},
+    status: {type: Number, default: 1},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
 });
 
 export default mongoose.model('User', user);
