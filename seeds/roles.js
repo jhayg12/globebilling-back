c2a968f7075bcc5e564df293b75b20ee8cf8f792
Jayson Suyat <jayson@iscale-solutions.com>
2018-07-18 11:58:19 +0800

    Update Roles Seeder File

--- seeds/roles.js
+++ seeds/roles.js
@@ -4,14 +4,14 @@ module.exports = [
     {   
         "_id" : new mongoose.Types.ObjectId("5b4be9d61550851c40ae84d2"),
         "permission" : [],
-        "roleName" : "admin",
+        "roleName" : "Admin",
         "createdAt" : "2018-07-16T00:41:58.293Z",
         "updatedAt" : "2018-07-16T00:41:58.293Z"
     },
     {   
         "_id" : new mongoose.Types.ObjectId("5b4d8267a83b1d4ff53f55d9"),
         "permission" : [],
-        "roleName" : "viewer",
+        "roleName" : "Viewer",
         "createdAt" : "2018-07-16T00:41:58.293Z",
         "updatedAt" : "2018-07-16T00:41:58.293Z"
     }
