e71391f68b3f136190a72c73ea55773c51d40f8e
Jayson Suyat <jayson@iscale-solutions.com>
2018-07-18 10:58:34 +0800

    Add Roles Seeder File

---
+++ seeds/roles.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+module.exports = [
+    {   
+        "_id" : new mongoose.Types.ObjectId("5b4be9d61550851c40ae84d2"),
+        "permission" : [],
+        "roleName" : "admin",
+        "createdAt" : "2018-07-16T00:41:58.293Z",
+        "updatedAt" : "2018-07-16T00:41:58.293Z"
+    },
+    {   
+        "_id" : new mongoose.Types.ObjectId("5b4d8267a83b1d4ff53f55d9"),
+        "permission" : [],
+        "roleName" : "viewer",
+        "createdAt" : "2018-07-16T00:41:58.293Z",
+        "updatedAt" : "2018-07-16T00:41:58.293Z"
+    }
+]
